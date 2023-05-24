015daf7dd8a3bf47db5a03c5247be091c1e7cb59
Kyle <kyleslaughter8@gmail.com>
2023-05-24 14:32:32 -0600

    finished* index.html and added basic functionality for quiz. Timer is broken.

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+var startEl = $('#startBtn');
+var questionsEl = $('#questions');
+var timeEl = $('#timer');
+var timer;
+var questionList = [{question: "does this work?", answers: ["poopoo", "peepee", "stinky"]}];
+var seconds = 60;
+var questionsAnswered = 0;
+var correct = 0;
+
+console.log(startEl);
+console.log(questionsEl);
+
+startEl.on('click', function (){
+    timer = setInterval(time(), 1000);
+    startEl.addClass('invisible');
+    questionsEl.addClass('p-1');
+    var newEl = $("<h1>" + questionList[questionsAnswered].question + "</h1>");
+    questionsEl.append(newEl);
+    console.log(questionList[questionsAnswered].question);
+    newEl.attr('class', 'bg-dark text-white p-3 text-center rounded');
+    for(var i = 0; i < questionList[questionsAnswered].answers.length; i++){
+        newEl = $("<p>" + questionList[questionsAnswered].answers[i] + "</p>");
+        questionsEl.append(newEl);
+        console.log(newEl);
+        newEl.attr('class', 'bg-info text-white text-center rounded mx-auto w-50 p-1');
+    }
+});
+
+function time(){
+    seconds--;
+    timeEl.text(seconds);
+    console.log("time ran");
+}
