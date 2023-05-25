2693f8bd7217cb5da7250ec1378d74b1273caa85
Kyle <kyleslaughter8@gmail.com>
2023-05-24 18:49:56 -0600

    Finished* README.md and added comments to script.js

---
+++ README.md
@@ -0,0 +1,18 @@
+## Coding quiz
+
+# Goal
+
+The goal of this project was to make a quiz about coding.
+The criteria was to have a start button for the quiz that would initiate a timer and present the user with questions.
+Although there was no criteria about user feedback, a response of "correct" or "incorrect" was added.
+
+# Functionality
+
+The user loads the page and is presented with a very minimal layout, with only a "start" button and a few things in the header.
+Once the start button is pressed, a timer in the top right counts down from 60 and the quiz goes through 13 questions.
+The raw question data can be found in the script.js. The answers to the questions are randomly presented. I did this to ensure that no one could cheese the quiz by always pressing the first answer (I designed my code to always have the first answer correct).
+When the quiz is over, the user is presented with previous scores and a way to submit their score. The user is supposed to only enter initials in that field but I was too lazy to do validation for it.
+
+# Conclusion
+
+This was a fun javascript task and it feels pretty great to make my first (really) functional thing. I can send the link to my friends and they can play my quiz. Most other projects I have made have required them to have compilers or some other way to view it.
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
+// Inital jQuery grabs
 var startEl = $('#startBtn');
 var questionsEl = $('#questions');
 var timeEl = $('#timer');
 
+// var to run the timer
 var timer;
+
+// raw question data, can be expanded. The first answer in the answers array is always the correct one. They are displayed randomly later.
 var questionList = [{question: "What is CSS?", answers: ["Cascading Styling Sheets", "Really really bad", "A military officer", "Hyper-Text Markup Language"]},
                     {question: "What is JavaScript?", answers: ["The language of the web", "Really really bad", "A war document", "A resturaunt"]},
                     {question: "What is HTML?", answers: ["Hyper-Text Markup Language", "Really really bad", "An indian dish", "A resturaunt"]},
@@ -16,82 +20,84 @@ var questionList = [{question: "What is CSS?", answers: ["Cascading Styling Shee
                     {question: "Is this question hard?", answers: ["No", "Yes", "What?", "No it's very soft"]},
                     {question: "What is my favorite show?", answers: ["Better Call Saul", "Breaking Bad", "Attack on Titan", "House MD"]},
                     {question: "Are you cheating?", answers: ["Yes", "No", "I don't know how to cheat", "None of your business"]}];
-var seconds = 60;
+
+// vars to store game data
 var questionsAnswered = 0;
 var correctAnswers = 0;
 var inGame = false;
 
-// console.log(startEl);
-// console.log(questionsEl);
-
+// event handler for start button
 startEl.on('click', function (){
     inGame = true;
     seconds = 61;
+
+    // starts the timer
     time();
     timer = setInterval(time, 1000);
 
+    // makes the start button invisible
     startEl.addClass('invisible');
-    questionsEl.addClass('p-1');
 
     // Display question
     displayQuestion();
 });
 
 function displayQuestion () {
+    // sees if the player has gone through all the questions
     if(questionList[questionsAnswered] == undefined){
         playerWon();
         return;
     }
+
+    // Displays the question to the user
     var newEl = $("<h1>" + questionList[questionsAnswered].question + "</h1>");
     questionsEl.append(newEl);
-    // console.log(questionList[questionsAnswered].question);
-
     newEl.attr('class', 'bg-dark text-white p-3 text-center rounded');
 
     //Display answers below question
     var answersCopy = [];
+    
+    // for loop to avoid storing a reference
+    // individually assigns each index
     for(var i = 0; i < 4; i++){
         answersCopy[i] = questionList[questionsAnswered].answers[i];
     }
+
+    // uses the copy of the answers array to randomly select an answer to display, then removes it from the copy for the next iteration
     for(var i = 0; i < 4; i++){
         var random = Math.floor(Math.random() * answersCopy.length);
+
         newEl = $("<p>" + answersCopy[random] + "</p>"); // Create new element with random answer
-        // console.log(answersCopy);
-        // console.log(questionList[questionsAnswered]);
-        answersCopy.splice(random, 1);
         questionsEl.append(newEl);
-        // console.log(newEl);
         newEl.attr('class', 'bg-info text-white text-center rounded mx-auto w-50 p-1');
+
+        answersCopy.splice(random, 1); // Removes chosen answer from the array copy
     }
-    
 }
 
+// Event handler for clicking on an answer
 questionsEl.on('click', function (event){
     if(inGame){
-        var clicked = event.target.textContent;
-        // console.log(clicked);
-        // console.log(questionsAnswered);
-        // console.log(questionList[questionsAnswered]);
-        // console.log(questionList[questionsAnswered].answers);
-        // console.log(questionList[questionsAnswered].answers[0] === clicked);
-        var correctText;
+        var clicked = event.target.textContent; // gathers the text from the answer that was clicked on
+        var correctText; // Stores the text that is later displayed
 
-        if(questionList[questionsAnswered].answers[0] === clicked){
-            // console.log("Correct answer branch");
+        if(questionList[questionsAnswered].answers[0] === clicked){ // Correct 
             correctAnswers++;
             correctText = "Correct!";
-        }else{
-            // console.log("Wrong answer branch");
+        }else{                                                      // Incorrect
             seconds -= 10;
             correctText = "Incorrect! -10 seconds";
         }
         questionsAnswered++;
-        questionsEl.empty();
 
+        questionsEl.empty(); // Removes the answers
+
+        // Displays to the user whether they got it right or wrong
         var correctEl = $("<p>" + correctText + "</p>");
         questionsEl.append(correctEl);
         correctEl.attr('class', 'bg-info text-white text-center rounded mx-auto w-50');
 
+        // Waits 1 second before removing the text and displaying the next question
         setTimeout(function() {
             if(inGame){
                 questionsEl.empty();
@@ -101,11 +107,12 @@ questionsEl.on('click', function (event){
     }
 });
 
+// Timer
 function time(){
     seconds--;
     timeEl.text(seconds);
-    // console.log("time ran");
-    if(seconds <= 0){
+
+    if(seconds <= 0){ // If time runs out, quiz is over and scores are displayed
         timeEl.text("0");
         clearInterval(timer);
         inGame = false;
@@ -120,7 +127,9 @@ function time(){
     }
 }
 
-function playerWon() {
+// If questions run out, quiz is over and scores are displayed
+// Different text is displayed than when time runs out
+function playerWon() { 
     timeEl.text("0");
     clearInterval(timer);
     inGame = false;
@@ -134,45 +143,66 @@ function playerWon() {
     displayScores();
 }
 
+// Shows a list of previous scores and allows the user to save their score into that list
 function displayScores(){
+    // Creates a section for the list and input field
     var sectionEl = $("<section>");
     questionsEl.append(sectionEl);
     sectionEl.attr('class', 'bg-white rounded p-2 w-100 d-flex flex-column justify-content-between');
+
     var newEl = $('<h1>Scores</h1>');
     sectionEl.append(newEl);
     newEl.attr('class', 'text-center');
+
+    // List items of scores are children of this
     var listEl = $('<ul>');
     sectionEl.append(listEl);
-    var scores = JSON.parse(localStorage.getItem("scores"));
-    if(scores !== null){
-        for(var i = 0; i < scores.length; i++){
+
+    var scores = JSON.parse(localStorage.getItem("scores")); // Grab old scores from localStorage
+    if(scores !== null){ // Makes sure that scores is not empty to avoid any issues
+        for(var i = 0; i < scores.length; i++){ // Traverses scores and adds list items to the screen
             var itemEl = $("<li>" + scores[i].player + ": " + scores[i].score + "</li>");
             listEl.append(itemEl);
             itemEl.attr('class', 'text-center p-2');
         }
-    }else{
+    }else{ // If scores is empty, we initialize it to an empty array
         scores = [];
     }
+
+    // Creates the form element for user to save score
     var formEl = $("<form>");
     formEl.attr('class', 'mx-auto');
     sectionEl.append(formEl);
+
+    // Creates the label for the input field
     var textEl = $("<label>Initials: </label>");
     textEl.attr('for', 'initals');
     formEl.append(textEl);
+
+    // Creates the input field
     var inputEl = $("<input>");
     inputEl.attr('name', 'initials');
     inputEl.attr('type', 'text');
     formEl.append(inputEl);
+
+    // Creates the submit button
     var btnEl = $("<button>Submit</button>");
     formEl.append(btnEl);
+
+    // Event handler for the submit button
     btnEl.on('click', function (event){
-        event.preventDefault();
-        console.log(inputEl.val());
-        scores.push({player: inputEl.val(), score: correctAnswers});
-        localStorage.setItem("scores", JSON.stringify(scores));
+        event.preventDefault(); // Prevents the page from refreshing
+
+        scores.push({player: inputEl.val(), score: correctAnswers}); // Adds the new score on to the list array
+
+        localStorage.setItem("scores", JSON.stringify(scores)); // Pushes scores to localStorage
+
+        // Adds the players new score on to the screen
         var itemEl = $("<li>" +  inputEl.val() + ": " + correctAnswers + "</li>");
         listEl.append(itemEl);
         itemEl.attr('class', 'text-center p-2');
+
+        // Removes the input field
         formEl.empty();
     });
 };
